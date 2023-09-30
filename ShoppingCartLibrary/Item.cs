2baa22713e567a5888ed2647eae93876c1e25f83
OlexandrBuhaienko <sasha2003bug@gmail.com>
2023-10-01 01:12:50 +0300

    First commit. Added class ShoppingCartLibrary and class Item

---
+++ ShoppingCartLibrary/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartLibrary
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public Item(string name = "", int quantity = 0)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ ShoppingCartLibrary/ShoppingCartLibrary.cs
@@ -0,0 +1,33 @@
+﻿namespace ShoppingCartLibrary
+{
+    [Serializable]
+    public class ShoppingCart : IDisposable
+    {
+        private List<Item> _items = new List<Item>();
+        public List<Item> Items
+        {
+            get
+            {
+                List<Item> returnedItems = new List<Item>(_items);
+                return returnedItems;
+            }
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+        public void Add(Item item)
+        {
+            _items.Add(item);
+        }
+        public void Remove(int index)
+        {
+            _items.RemoveAt(index);
+        }
+        public void Dispose()
+        {
+            //cleanup
+        }
+    }
+}
